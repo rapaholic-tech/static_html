af103123f8785b7177c8b3d79bc6c5f8f5021e58
rapaholic-tech <flopan88@gmail.com>
2025-05-21 20:20:56 +0200

    next

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode
 
 def main():
     node = TextNode("This is sample text", TextType.LINK, "https://example.com")
-
+    node2 = HTMLNode("div", "Hello, world!", None, {"class": "greeting", "href": "https://boot.dev"})
     print(node)
+    print(node2)
 
 if __name__ == "__main__":
     main()
